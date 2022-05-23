e9c9b41ca5da5f27229fdaf6bcbf3b90b83345c5
hyperliskdev <cartertomlenovich@gmail.com>
2022-05-23 04:23:41 -0400

    weird Egui Issue

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,15 @@ mod state;
 use bevy::prelude::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use state::main_menu::MainMenuPlugin;
-use bevy_egui::{egui, EguiContext, EguiPlugin};
-
-
-
+use bevy_egui::{EguiPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_plugin(EguiPlugin)
         .add_plugin(MainMenuPlugin)
         .run();
 }
+
--- src/state/main_menu.rs
+++ src/state/main_menu.rs
@@ -1,13 +1,32 @@
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext};
+
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup_menu);
-        app.add_system(system)
     }
 
 }
 
-fn setup_menu(mut commands: Commands, assets: Res<AssetServer>) {}
+fn setup_menu(mut egui_context: ResMut<EguiContext>) {
+
+    egui_context.ctx_mut().set_visuals(egui::Visuals {
+        window_rounding: 0.0.into(),
+        ..Default::default()
+    });
+        
+    egui::Area::new("Main Menu").movable(true).show(egui_context.ctx_mut(), |ui| {
+        ui.add(egui::Label::new("Start Game"));
+    });
+    
+    /*
+    There is the -?
+        camera.orthographic_projection.[top, bottom, left, right ... ]
+
+        possibly useful??
+    */
+    
+}
